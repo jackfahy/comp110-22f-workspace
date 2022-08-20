56d362ad67aaf9074966bb5152343576fd2bf118
Jack Fahy <jfahy@unc.edu>
2022-08-20 19:06:09 -0400

    Finished Exercise 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,4 +1,4 @@
-""My first program for COMP110.""
-_author_=730482932
+"""My first program for COMP110."""
+_author_= "730482932"
 
-print("Hello Jack, and welcome to the coding world.")
+print("Hello world.")

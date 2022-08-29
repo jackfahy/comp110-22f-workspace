685e8bdcc4fcf9feac37e46702e97e8c143f620e
Jack Fahy <jfahy@unc.edu>
2022-08-29 14:00:25 -0400

    "working on exercise 1"

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,4 +1,4 @@
-""""EX01 - Chardle - A cute step toward Wordle."""
+"""EX01 - Chardle - A cute step toward Wordle."""
 __author__="730482932"
 
 word: str=input("Enter a 5 character word: ")
@@ -13,19 +13,19 @@ instance: int=0
 
 print("Searching for " + character + " in "+ word)
 if character == word[0]:
-    print(character + " found at index 1 ")
+    print(character + " found at index 0 ")
     instance = 1
 if character == word[1]:
-    print(character + " found at index 2 ")
+    print(character + " found at index 1 ")
     instance = instance + 1
 if character == word[2]:
-    print(character+ " found at index 3 ")
+    print(character+ " found at index 2 ")
     instance = instance + 1
 if character == word[3]:
-    print(character+" found at index 4 ")
+    print(character+" found at index 3 ")
     instance = instance + 1
 if character == word[4]:
-    print(character+" found at index 5 ")
+    print(character+" found at index 4 ")
     instance = instance + 1
 if instance == 1:
     print(str(instance) + " instance of " + character + " found in " + word)
@@ -33,5 +33,4 @@ if instance > 1:
     print(str(instance) + " instances of " + character + " found in " + word)
 if instance == 0:
     print("No instances of "+ character + " found in " + word)
-
-
+    
